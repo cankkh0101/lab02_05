98ed68f7fd3c555b73770e9208db5b1dbfb06dc4
kim <cankkh0101@snu.ac.kr>
2018-04-09 12:25:58 +0900

    2017-19929

---
+++ 2017-19929.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void main() {
+	vector<int> list;
+	auto num = 1;
+	auto nam = 0;
+	while (cin >> num) {
+		list.push_back(num);
+	}
+	cout << "original array" << endl;
+	for (auto it = list.begin(); it < list.end(); it++) {
+		cout << *it << " ";
+
+	}
+	cout << endl << endl << "ordered array\n";
+
+	for (int j = 0; j < list.size(); j++){
+		for (int i = 0; i < list.size()-1; i++) {
+			if (list[i] <= list[i + 1]) {
+				nam = list[i];
+				list[i] = list[i + 1];
+				list[i + 1] = nam;
+			}
+			else {}
+
+
+		}
+}
+
+	for (auto i : list) {
+		cout << i << " ";
+	}
+	
+	cout << endl;
+	system("pause");
+}
